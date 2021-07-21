32b9b96c66e6a484c4d6e2e98aac1aace613d1e7
ztt <1261087861@qq.com>
2021-07-21 20:17:48 +0800

    hot fix first commit

---
+++ gtitest/src/main/java/com/ztt/git/TestDemo.java
@@ -0,0 +1,11 @@
+package com.ztt.git;
+
+/**
+ * @author ztt
+ * @Date: Created in 2021/7/21
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("git test!!");
+    }
+}
